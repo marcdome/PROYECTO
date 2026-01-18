e2ae5d0fe9d5b696a9a1002c19017da3176dc9b8
Marcdome <marcdonatomeri@gmail.com>
2026-01-18 22:45:43 +0100

    primera peticion

--- codigo.py
+++ codigo.py
@@ -1,4 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-#
+
+# Primera pagina web
+juego = input("Dime el juego que quieres consultar: ") # Pedimos el juego
+juego.replace(" ", "-")
+juego.lower()
+url = f"https://www.metacritic.com/game/{juego}/"
+contenido = requests.get(url)
+print(contenido.text)
