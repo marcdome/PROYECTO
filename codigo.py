f91f833c3b7090d7209f017bcd78ef04a4e08c95
Marcdome <marcdonatomeri@gmail.com>
2026-01-18 23:08:54 +0100

    utilizamos selenium en la primera por el bloqueo

--- codigo.py
+++ codigo.py
@@ -1,11 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import selenium
 
 # Primera pagina web
-juego = input("Dime el juego que quieres consultar: ") # Pedimos el juego
-juego.replace(" ", "-")
-juego.lower()
-url = f"https://www.metacritic.com/game/{juego}/"
-contenido = requests.get(url)
-print(contenido.text)
