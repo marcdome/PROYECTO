fddcec76b5ef2cae2969872149043f54eee6f834
Marcdome <marcdonatomeri@gmail.com>
2026-01-19 11:14:49 +0100

    solo lo necesario

--- codigo.py
+++ codigo.py
@@ -1,6 +1,17 @@
 import requests
-from bs4 import BeautifulSoup
 import pandas as pd
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+import json
+
+url  = "https://api.adzuna.com/v1/api/jobs/es/search/1"
+APP_ID = "99936c6b"
+API_KEY = "5b5d7a3ac709455f1022920b276dccf7"
+params = {
+    "app_id": APP_ID,
+    "app_key": API_KEY,
+    "what": "python",
+    "content-type": "application/json"
+}
+response = requests.get(url, params=params)
+print(response.status_code)
+print(json.dumps(response.json()))
 
