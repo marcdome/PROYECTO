62b623ae90040152d6cd4ce46852df77d9ccf156
Marcdome <marcdonatomeri@gmail.com>
2026-01-24 23:05:59 +0100

    mas analisis

--- analisis.py
+++ analisis.py
@@ -15,7 +15,7 @@ data.set_index('tecnologia')['demanda_total'].plot(kind='bar')
 plt.title("Demanda Total por Lenguaje de Programación")
 plt.xlabel("Lenguaje de programación")
 plt.ylabel("Demanda Total")
-plt.show()
+#plt.show()
 
 data2 = tiobe_data[["Lenguaje", "Porcentaje"]]
 data2["Porcentaje"] = data2["Porcentaje"].str.rstrip('%').astype(float)
@@ -24,7 +24,7 @@ data2.set_index('Lenguaje')['Porcentaje'].plot(kind = 'bar')
 plt.title('Posiciones de los lenguajes más populares')
 plt.xlabel("Lenguaje de programación")
 plt.ylabel("Popularidad")
-plt.show()
+#plt.show()
 
 # 2. ¿Los lenguajes mas populares tiene más repositorios?
 
@@ -34,5 +34,39 @@ data3.set_index('tecnologia')['github_repos'].plot(kind = 'bar')
 plt.title('Repositorios de Github por cada lenguaje')
 plt.xlabel('Lenguaje de programación')
 plt.ylabel('Total de repositorios')
+#plt.show()
+
+# 3. ¿Cual es la ciudad que tiene mas ofertas?
+
+ciudades_filtradas = azuna_data[~azuna_data["ubicacion"].str.contains("España", na=False)]
+ciudades_filtradas = ciudades_filtradas["ubicacion"]
+print(ciudades_filtradas)
+def transformacion(p:str):
+    parts = p.split(',')
+    if len(parts) == 1:
+        return parts[0].strip()
+    else:
+        for c in range(len(parts)):
+           parts[c] = parts[c].strip()
+        if len(parts[0]) > len(parts[1]):
+            if parts[0] == "Barcelona":
+                return parts[0]
+            return parts[1]
+        else:
+            return parts[0]
+
+ciudades_limpias = ciudades_filtradas.apply(transformacion)
+conteo = ciudades_limpias.value_counts(ascending=False).head(10)
+df_ciudades = conteo.reset_index(name="total")
+print(df_ciudades)
+
+df_ciudades.set_index('ubicacion')['total'].plot(kind= 'bar')
+plt.title('Ciudades con más ofertas de trabajo')
+plt.xlabel('Ciudades')
+plt.ylabel('Total')
 plt.show()
 
+
+
+
+    
